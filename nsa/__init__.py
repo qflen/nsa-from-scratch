b55065073686da6f8426deaf6b4d9accd0ad660b
qflen <nkimon00@gmail.com>
2026-01-31 22:19:36 +0100

    chore: package docstring + __version__ for v0.1.0

--- nsa/__init__.py
+++ nsa/__init__.py
@@ -1,1 +1,7 @@
-"""NSA: Native Sparse Attention reimplementation."""
+"""NSA-from-scratch: Native Sparse Attention reimplementation.
+
+Reference: Yuan et al., "Native Sparse Attention: Hardware-Aligned and Natively
+Trainable Sparse Attention", arXiv 2502.11089 (February 2025).
+"""
+
+__version__ = "0.1.0"
